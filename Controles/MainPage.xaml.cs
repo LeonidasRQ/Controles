6f420c8d5ea89b25f5159b428f3ab1084fd6f544
Leonidas <leonidas.ramirez.q@gmail.com>
2021-02-16 15:51:38 -0500

    El profe borró todo en el ejercicio, así que comenzaré otra vez

--- Controles/MainPage.xaml.cs
+++ Controles/MainPage.xaml.cs
@@ -13,29 +13,6 @@ namespace Controles
         public MainPage()
         {
             InitializeComponent();
-            dtp.Date = DateTime.Now;
-            dtp.MinimumDate = new DateTime(1990,01,01);
-            dtp.MaximumDate = new DateTime(2020, 06, 15);
-            dtp.DateSelected += Dtp_DateSelected;
-        }
-
-        private void Dtp_DateSelected(object sender, DateChangedEventArgs e)
-        {
-            DisplayAlert("Fecha", e.NewDate.ToString(), "Cerrar");
-        }
-
-        private void Button_Clicked(object sender, EventArgs e) {
-            DisplayAlert("Saludo", "Hola como estás?", "Cerrar");
-        }
-
-        private void Entry_Completed(object sender, EventArgs e)
-        {
-            DisplayAlert("Completed", "Texto Completado", "Cerrar");
-        }
-
-        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            DisplayAlert("TextChanged", "Cambio", "Cerrar");
-        }
+            }
     }
 }
